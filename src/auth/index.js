39d026a11cbebddcec1672aac4f747347f426bc2
Kameron Watts <kam.watts@gmail.com>
2023-05-17 21:54:28 -0700

    added auth, login, and roles capabilities

---
+++ src/auth/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Auth from './auth';
+import Login from './login.js';
+import LoginContext from './context.js';
+
+function App() {
+  return (
+    <LoginContext>
+      <Login />
+
+      <Auth>
+        <div>Any valid user can see this</div>
+      </Auth>
+
+      <Auth capability="create">
+        <div>Users with create access can see this</div>
+      </Auth>
+
+      <Auth capability="update">
+        <div>Users with update access can see this</div>
+      </Auth>
+
+      <Auth capability="delete">
+        <div>Users with delete access can see this</div>
+      </Auth>
+    </LoginContext>
+  );
+}
+
+export default App;
